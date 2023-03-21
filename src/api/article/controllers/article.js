3663bbe58d05754145fa2e1bd83f7b8cd53919b0
Nodil1 <nodilka1@gmail.com>
2023-03-21 14:01:33 +0300

    Add Likes and Views

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -4,6 +4,52 @@
  *  article controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const {createCoreController} = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController('api::article.article', ({strapi}) => ({
+  async logView(ctx) {
+    const {slug} = ctx.params;
+    const {query} = ctx;
+
+    if (!query.filters) query.filters = {}
+    query.filters.slug = {'$eq': slug}
+
+    const article = await strapi.service('api::article.article').find(query);
+    const entry = await strapi.db.query('api::article.article').findOne({
+      where: {slug: slug},
+    });
+    await strapi.db.query('api::article.article').update({
+      where: {slug: slug},
+      data: {
+        views: parseInt(entry.views) + 1,
+      },
+    });
+    return ctx.send({
+      success: true,
+      ar: entry.views
+    });
+  },
+
+  async logLike(ctx) {
+    const {slug} = ctx.params;
+    const {query} = ctx;
+
+    if (!query.filters) query.filters = {}
+    query.filters.slug = {'$eq': slug}
+
+    const article = await strapi.service('api::article.article').find(query);
+    const entry = await strapi.db.query('api::article.article').findOne({
+      where: {slug: slug},
+    });
+    await strapi.db.query('api::article.article').update({
+      where: {slug: slug},
+      data: {
+        likes: parseInt(entry.likes) + 1,
+      },
+    });
+    return ctx.send({
+      success: true,
+      ar: entry.likes
+    });
+  },
+}));
--- src/api/article/routes/article.js
+++ src/api/article/routes/article.js
@@ -6,4 +6,50 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::article.article');
+const defaultRouter = createCoreRouter('api::article.article');
+
+const customRouter = (innerRouter, routeOveride = [], extraRoutes = []) => {
+  let routes;
+
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes;
+
+      const newRoutes = routes.map((route) => {
+        let found = false;
+
+        routeOveride.forEach((overide) => {
+          if (
+            route.handler === overide.handler &&
+            route.method === overide.method
+          ) {
+            found = overide;
+          }
+        });
+
+        return found || route;
+
+      });
+
+      return newRoutes.concat(extraRoutes);
+    },
+  };
+};
+
+const myExtraRoutes = [
+  {
+    method: "GET",
+    path: "/articles/:slug/view",
+    handler: "article.logView",
+  },
+  {
+    method: "GET",
+    path: "/articles/:slug/like",
+    handler: "article.logLike",
+  }
+]
+
+module.exports = customRouter(defaultRouter, [], myExtraRoutes);
